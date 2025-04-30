3539ff0443ffb27ecefdecaba90637284037800e
brayanBorda <144700047+brayanBorda@users.noreply.github.com>
2025-04-30 00:10:31 -0500

    fix: ajustes en OrderService

--- pedidosapi/src/main/java/com/virtualcoffee/pedidosapi/service/OrderService.java
+++ pedidosapi/src/main/java/com/virtualcoffee/pedidosapi/service/OrderService.java
@@ -22,7 +22,7 @@ public class OrderService {
         this.beverageClient = beverageClient;
     }
 
-    //añadir
+    //añadir a
     public Order addOrder(Order order) {
         // Validamos si la bebida existe
         if (!beverageClient.isBeverageAvailable(order.getName())) {
