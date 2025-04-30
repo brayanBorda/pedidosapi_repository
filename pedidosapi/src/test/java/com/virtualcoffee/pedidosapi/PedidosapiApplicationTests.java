8577f005856e9af0372e292a591c88b04416c2cd
brayanBorda <144700047+brayanBorda@users.noreply.github.com>
2025-04-29 22:15:26 -0500

    Integración con SonarCloud

--- pedidosapi/src/test/java/com/virtualcoffee/pedidosapi/PedidosapiApplicationTests.java
+++ pedidosapi/src/test/java/com/virtualcoffee/pedidosapi/PedidosapiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.virtualcoffee.pedidosapi;
 
+import com.virtualcoffee.pedidosapi.service.BeverageClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.context.TestConfiguration;
 
 @SpringBootTest
 class PedidosapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @TestConfiguration
+    static class MockConfig {
+        @Bean
+        public BeverageClient beverageClient() {
+            return name -> true; // Simula que siempre hay disponibilidad
+        }
+    }
 }
